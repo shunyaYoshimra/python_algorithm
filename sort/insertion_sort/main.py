b89f2fb2b41cea34d4bef92cfe88d47b192cc608
shunyaYoshimura <maoorgri1015@gmail.com>
2021-10-30 22:35:00 +0900

    learn insertion_sort

---
+++ sort/insertion_sort/main.py
@@ -0,0 +1,26 @@
+# インサーションソート
+
+from typing import List
+
+
+def getNums() -> List[int]:
+    str_list = [s for s in input().split()]
+    nums = list(map(int, str_list))
+    return nums
+
+
+def insertion_sort(numbers: List[int]) -> List[int]:
+    nums_length = len(numbers)
+    for i in range(1, nums_length):
+        temp = numbers[i]
+        j = i - 1
+        while j >= 0 and numbers[j] > temp:
+            numbers[j+1] = numbers[j]
+            j -= 1
+        numbers[j+1] = temp
+    return numbers
+
+
+if __name__ == '__main__':
+    nums = getNums()
+    print(insertion_sort(nums))
