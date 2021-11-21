1adea136b44fda3e39ce591a3ece340904278915
shunyaYoshimura <maoorgri1015@gmail.com>
2021-11-21 17:43:03 +0900

    wrote comb sort self

--- sort/comb_sort/self2.py
+++ sort/comb_sort/self2.py
@@ -10,18 +10,19 @@ def getNums() -> List[int]:
 def comb_sort(numbers: List[int]) -> List[int]:
     nums_length = len(numbers)
     gap = nums_length
-    swapped = False
+    swapped = True
 
     while gap != 1 or swapped:
         gap = int(gap / 1.3)
         if gap < 1:
             gap = 1
 
-        for i in range(0, numbers - gap):
-            if numbers[i] > numbers[i + gap]:
-                numbers[i], numbers[i + gap] = numbers[i + gap], numbers[i]
-                swapped = True
+        swapped = False
 
+        for i in range(0, nums_length - gap):
+            if numbers[i] > numbers[i + gap]:
+                numbers[i], numbers[i+gap] = numbers[i+gap], numbers[i]
+                swapped = True
     return numbers
 
 
