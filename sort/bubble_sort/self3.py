de661036b9897c1dd1d1713515bf8d6910192442
shunyaYoshimura <maoorgri1015@gmail.com>
2021-12-01 19:37:28 +0900

    wrote bubble_sort

---
+++ sort/bubble_sort/self3.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+def getNums() -> List[int]:
+    str_list = [s for s in input().split()]
+    nums = list(map(int, str_list))
+    return nums
+
+
+def bubble_sort(numbers: List[int]) -> List[int]:
+    nums_length = len(numbers)
+    for i in range(nums_length-1):
+        for j in range(nums_length-i-1):
+            if numbers[j] > numbers[j+1]:
+                numbers[j], numbers[j+1] = numbers[j+1], numbers[j]
+    return numbers
+
+
+if __name__ == '__main__':
+    nums = getNums()
+    print(bubble_sort(nums))
