aaa85a9fc3a0601cfb198b596bcfdaea66770b46
shunyaYoshimura <maoorgri1015@gmail.com>
2021-12-03 21:50:23 +0900

    wrote comb_sort

---
+++ sort/comb_sort/self3.py
@@ -0,0 +1,29 @@
+from typing import List
+
+
+def getNums() -> List[int]:
+    str_list = [s for s in input().split()]
+    nums = list(map(int, str_list))
+    return nums
+
+
+def comb_sort(numbers: List[int]) -> List[int]:
+    nums_length = len(numbers)
+    gap = nums_length
+    swapped = True
+
+    while gap != 1 or swapped:
+        gap = int(gap/1.3)
+        if gap < 1:
+            gap = 1
+        swapped = False
+        for i in range(nums_length - gap):
+            if numbers[i] > numbers[i+gap]:
+                numbers[i], numbers[i+gap] = numbers[i+gap], numbers[i]
+                swapped = True
+    return numbers
+
+
+if __name__ == '__main__':
+    nums = getNums()
+    print(comb_sort(nums))
